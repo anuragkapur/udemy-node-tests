41ef67376a63270c266cdfff829a938488ec0d0e
Anurag Kapur <anurag@beancrunch.com>
2019-02-21 08:33:06 +0000

    Organise server tests using describe()

--- server/server.test.js
+++ server/server.test.js
@@ -2,15 +2,19 @@ const expect = require('expect');
 const request = require('supertest');
 const app = require('./server').app;
 
-it('should get users and the response should contain expected user in users array', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((response) => {
-      expect(response.body).toInclude({
-        name: 'Anurag Kapur',
-        location: 'London'
-      })
-    })
-    .end(done);
+describe('Server', () => {
+  describe('#Get Users', () => {
+    it('should get users and the response should contain expected user in users array', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body).toInclude({
+            name: 'Anurag Kapur',
+            location: 'London'
+          })
+        })
+        .end(done);
+    });
+  });
 });
